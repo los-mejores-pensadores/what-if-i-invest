6cb708c3288a484bfd21cbe3466b8fa83bb9d2cd
kfirstri <kfirstar@gmail.com>
2017-12-30 18:56:51 +0200

    added shit

--- ui/src/CoinSelector.js
+++ ui/src/CoinSelector.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import StarBorder from 'material-ui/svg-icons/toggle/check-box-outline-blank';
+import Checkbox from 'material-ui/Checkbox';
 
 const styles = {
   root: {
@@ -15,41 +16,55 @@ const styles = {
     overflowX: 'auto',
   },
   titleStyle: {
-    color: 'rgb(255, 255, 255)',
+    color: 'rgb(0, 0, 0)',
   },
 };
 
-const tilesData = [
-  {
-    img: 'https://png.icons8.com/dusk/2x/85cbf8/bitcoin.png',
-    title: 'Bitcoin',
-  },
-  {
-    img: 'https://png.icons8.com/dusk/2x/85cbf8/bitcoin.png',
-    title: 'Bitcoin',
-  },
-  {
-    img: 'https://png.icons8.com/dusk/2x/85cbf8/bitcoin.png',
-    title: 'Bitcoin',
-  },
-];
+class SingleLineCoinSelector extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      coins: this.loadCoins(),
+      selected_coins: []
+    }
+  }
 
-const SingleLineCoinSelector = () => (
-  <div style={styles.root}>
-    <GridList style={styles.gridList} cols={1.5}>
-      {tilesData.map((tile) => (
-        <GridTile
+  loadCoins() {
+    return [
+      {
+        img: 'https://png.icons8.com/dusk/2x/85cbf8/bitcoin.png',
+        title: 'Bitcoin',
+      },
+      {
+        img: 'https://png.icons8.com/dusk/2x/85cbf8/bitcoin.png',
+        title: 'Bitcoin',
+      },
+      {
+        img: 'https://png.icons8.com/dusk/2x/85cbf8/bitcoin.png',
+        title: 'Bitcoin',
+      },
+    ];
+  }
+
+  render() {
+    return (
+      <div style={styles.root}>
+        <GridList style={styles.gridList} cols={1.5}>
+        {this.state.coins.map((tile) => (
+          <GridTile
           key={tile.img}
           title={tile.title}
-          actionIcon={<IconButton><StarBorder color="rgb(255,255,255)" /></IconButton>}
+          actionIcon={<Checkbox iconStyle={styles.titleStyle} />}
           titleStyle={styles.titleStyle}
-          titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 60%,rgba(0,0,0,0) 100%)"
-        >
-          <img src={tile.img} />
-        </GridTile>
-      ))}
-    </GridList>
-  </div>
-);
+          titleBackground="none"
+          >
+          <img src={tile.img} alt="{tile.title}" />
+          </GridTile>
+        ))}
+        </GridList>
+      </div>
+    );
+  }
+}
 
 export default SingleLineCoinSelector;
